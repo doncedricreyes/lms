8ac52c1dad02274b80f1b4746946ca5140093d0a
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-08-29 02:43:25 +0800

    Update user.blade.php

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -91,10 +91,10 @@ desired effect
     <!-- Logo -->
     <a class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><img src="/storage/images/sccv.jpg" class="img-circle" id="logo" height="50" width="50"></span>
+      <span class="logo-mini"><img src="/storage/images/sccv.jpg" class="img-circle" id="logo" height="45" width="45"></span>
       <!-- logo for regular state and mobile devices -->
 
-      <span class="logo-lg">    <img src="/storage/images/sccv.jpg" class="img-circle" id="logo" height="50" width="70"></span>
+      <span class="logo-lg">    <img src="/storage/images/sccv.jpg" class="img-circle" id="logo" height="45" width="45"></span>
     </a>
 
     <!-- Header Navbar -->
