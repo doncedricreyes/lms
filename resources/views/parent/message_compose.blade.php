1cbe969d45d202d205997e557a3e988a4db7cf3c
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-08-29 00:33:33 +0800

    Update message_compose.blade.php

--- resources/views/parent/message_compose.blade.php
+++ resources/views/parent/message_compose.blade.php
@@ -19,6 +19,23 @@
 
 
 <div class="container" id="view">
+   <div class="flash-message">
+                            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                              @if(Session::has('alert-' . $msg))
+                        
+                              <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+                              @endif
+                            @endforeach
+                          </div> <!-- end .flash-message -->
+      @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
         <section class="content">
                         <div class="row">
                           <div class="col-md-3">
@@ -80,14 +97,7 @@
                                         </div>
                     </div>
                                       </form>
-                    <div class="flash-message">
-                            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-                              @if(Session::has('alert-' . $msg))
-                        
-                              <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
-                              @endif
-                            @endforeach
-                          </div> <!-- end .flash-message -->
+                 
             </div>
             
 
