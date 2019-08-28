d0c158e6c8d0fd1e9486203ef7e758c0c6b01618
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-08-28 23:02:18 +0800

    Update and rename NotifyMessage.php to NewMessage.php

---
+++ app/Notifications/NewMessage.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use App\Notifications\NewMessage;
+use Illuminate\Notifications\Notifiable;
+use App\Message;
+use Auth;
+
+class NewMessage extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+   
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {  
+         if(Auth::user()->role == 'student'){
+        return (new MailMessage)
+     
+                    ->line(Auth::user()->name)
+                    ->line('You have a new message.')         
+                    ->line('Please login to LMS to view your grade.')  
+                    ->action('Login to LMS', route('student.login'));
+    }  
+    if(Auth::user()->role == 'admin'){
+        return (new MailMessage)
+     
+                    ->line(Auth::user()->name)
+                    ->line('You have a new message.')         
+                    ->line('Please login to LMS to view your grade.')  
+                    ->action('Login to LMS', route('admin.login'));
+    } 
+    if(Auth::user()->role == 'teacher'){
+        return (new MailMessage)
+     
+                    ->line(Auth::user()->name)
+                    ->line('You have a new message.')         
+                    ->line('Please login to LMS to view your grade.')  
+                    ->action('Login to LMS', route('teacher.login'));
+    } 
+    if(Auth::user()->role == 'parent'){
+        return (new MailMessage)
+     
+                    ->line(Auth::user()->name)
+                    ->line('You have a new message.')         
+                    ->line('Please login to LMS to view your grade.')  
+                    ->action('Login to LMS', route('parent.login'));
+    } 
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
