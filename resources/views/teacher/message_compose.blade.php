cb0798a4254d09c3968a76d9310e3964b738d8aa
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-08-28 21:51:11 +0800

    Update message_compose.blade.php

--- resources/views/teacher/message_compose.blade.php
+++ resources/views/teacher/message_compose.blade.php
@@ -24,6 +24,14 @@
 
 
 <div class="container" id="view">
+      <div class="flash-message">
+                            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                              @if(Session::has('alert-' . $msg))
+                        
+                              <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+                              @endif
+                            @endforeach
+                          </div> <!-- end .flash-message -->
         <section class="content">
                         <div class="row">
                           <div class="col-md-3">
@@ -85,14 +93,7 @@
                                         </div>
                     </div>
                                       </form>
-                    <div class="flash-message">
-                            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-                              @if(Session::has('alert-' . $msg))
-                        
-                              <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
-                              @endif
-                            @endforeach
-                          </div> <!-- end .flash-message -->
+              
             </div>
             
 
