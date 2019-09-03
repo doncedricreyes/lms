8f421e840ca4f7b40f466907e8b804b7f74e8a5f
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-04 00:11:17 +0800

    Update students.blade.php

--- resources/views/admin/students.blade.php
+++ resources/views/admin/students.blade.php
@@ -28,7 +28,9 @@
                         
                         <div class="col-md-12">
                         <legend>Students</legend>
-            
+                 <form action = "{{route('search_student')}}" role="search" method="get"enctype="multipart/form-data">
+                            <input type="text" class="form-control" name="search" id="search" placeholder="Search">
+                                </form>
                         <div class="table-responsive">
                 
                                 
