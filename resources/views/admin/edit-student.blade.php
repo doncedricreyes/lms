ffba08c871e68cb2903e572dfc4640804e2ab1dd
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-04 18:41:07 +0800

    Update edit-student.blade.php

--- resources/views/admin/edit-student.blade.php
+++ resources/views/admin/edit-student.blade.php
@@ -39,11 +39,9 @@
                         <label for="name">Student's Name:</label>
                         <input type="text" name="name" id="name" value="{{$students->get(0)->name}}" class="form-control"> 
                         <label for="year">Year:</label>
-                         @foreach($class_students as $class_student)
                         <input type="text" name="year" id="year"  value="{{$class_student->class_subject_teachers->get(0)->classes->get(0)->year}}" class="form-control"> 
                         <label for="section">Section:</label>
                         <input type="text" name="section" id="section" value="{{$class_student->class_subject_teachers->get(0)->classes->get(0)->section}}"  class="form-control">
-                        @endforeach
                         <label for="username"> Username:</label>
                         <input type="text" name="username" id="username" value="{{$students->get(0)->username}}" class="form-control">
                         <label for="password"> Create a password:</label>
