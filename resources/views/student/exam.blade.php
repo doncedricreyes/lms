0cf17048f9941683c4c8a6bce32f9acdd526c064
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-06 17:32:17 +0800

    Update exam.blade.php

--- resources/views/student/exam.blade.php
+++ resources/views/student/exam.blade.php
@@ -27,7 +27,7 @@
         @foreach($exams as $id)
         @if ($id->date_start <= Carbon\Carbon::now('Asia/Manila')) 
         @if ($id->date_end > Carbon\Carbon::now('Asia/Manila')) 
-        @if($id->attempts > $exam_grades->attempt )
+        @if($id->attempts > $exam_grades->get(0)['attempt'] )
      
         
           <a href="{{route('student.show.question',$id->id)}}" class="btn btn-primary">Attempt Quiz/Exam</a>       
