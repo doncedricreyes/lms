10234cea40ab297341daeb65cf4cf5d0594ce233
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-06 17:09:31 +0800

    Update AnswerController.php

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Answer;
 use App\Question;
 use App\Student;
+use App\Exam_Grade;
+use App\Exam;
 use Auth;
 
 use Illuminate\Http\Request;
@@ -25,10 +27,17 @@ class AnswerController extends Controller
    public function store(Request $request,$id)
    {
     $questions = Question::with('exams')->where('exam_id',$id)->get();
-    
-  
-    
+    $exam_grades = Exam_Grade::with('exams','students')->where('student_id','=',Auth::user()->id)->where('exam_id','=',$id)->get();  
+    $exams = Exam::where('id','=',$id)->get();
    
+    if(count($exam_grades)>0){
+        foreach($exam_grades as $exam_attempt){
+    $attempt=$exam_attempt->attempt+1;
+        }
+    }
+    else{
+        $attempt=1;
+    }
          
 
         
@@ -37,11 +46,15 @@ class AnswerController extends Controller
         $answer->answer = $request->answer;
         $answer->question_id = $request->id;
         $answer->exam_id=$id;
+        $answer->attempt = $attempt;
+
+        if($exams->get(0)->attempts >= $attempt){
         $answer->save();
-   
-     
-            return redirect()->route('student.show.question',['id'=>$id]);
-            
+        return redirect()->route('student.show.question',['id'=>$id]);
+        }
+        else{
+            return redirect()->back()->withErrors('You have already attempted this exam/quiz!');
+        }    
 
    
    }
