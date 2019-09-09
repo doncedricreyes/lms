285de86b15249d635b4a07e062e4e22a474ebb76
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-10 00:36:13 +0800

    Update exam.blade.php

--- resources/views/student/exam.blade.php
+++ resources/views/student/exam.blade.php
@@ -20,6 +20,7 @@
 <h3>Total Score: {{$exam->total_score}}</h3>
 <h3>Passing Score: {{$exam->passing_score}}</h3>
 <h3>Attempts: {{$exam->attempts}}</h3>
+<h3>Your Attempt: {{$quiz_attempt->count()}}</h3>
 <h3>Time: {{$exam->time/60}} minutes</h3>
 
 
@@ -27,17 +28,24 @@
         @foreach($exams as $id)
         @if ($id->date_start <= Carbon\Carbon::now('Asia/Manila')) 
         @if ($id->date_end > Carbon\Carbon::now('Asia/Manila')) 
-        @if($id->attempts > $exam_grades->get(0)['attempt'] )
-     
-        
-          <a href="{{route('student.show.question',$id->id)}}" class="btn btn-primary">Attempt Quiz/Exam</a>       
-        @else <h3> Exam is  closed </h3> 
+        @if($id->attempts > $exam_grades->attempt )
+        @if($quiz_latest->id == $exam_grades->quiz_attempt_id) 
+        @if($quiz_attempt->count() < $id->attempts)    
+        <form action = "{{route('student.store.quiz_start',$id->id)}}" method="post" enctype="multipart/form-data">
+          {{csrf_field() }}
+       
+          <input type="submit" id="submit" class="btn btn-primary" value="Attempt Quiz/Exam">     
+          @endif
+         
     
         @endif
 @endif
 @endif
+@endif
+@if($quiz_latest->id != $exam_grades->quiz_attempt_id) 
 
-
+<a href="{{route('student.show.question',$id->id)}}" class="btn btn-primary">Continue</a>
+@endif
         <a href="{{route('student.show.result',$id->id)}}" class="btn btn-primary">View Results</a>
           @endforeach
 </div>
