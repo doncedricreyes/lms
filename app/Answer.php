19ba04af00050dca0c654d4e66098d4562d54f20
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-10 00:18:06 +0800

    Update Answer.php

--- app/Answer.php
+++ app/Answer.php
@@ -9,9 +9,9 @@ class Answer extends Model
     protected $table = 'answers';
     public $timestamps = false;
 
-public function students(){
+public function quiz_attempt(){
      
-    return $this->hasMany(Student::class,'id','student_id');
+    return $this->hasMany(Quiz_Attempt::class,'id','quiz_attempt_id');
     
 }
 public function questions(){
@@ -19,10 +19,6 @@ public function questions(){
     return $this->hasMany(Question::class,'id','question_id');
     
 }
-public function exams()
-{
-    return $this->hasMany(Exam::class,'id','exam_id');
 
-}
   
 }
