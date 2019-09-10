211c2390a96f891220efafc01dc4f5c285a4ba1c
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-10 21:33:40 +0800

    Update exam.blade.php

--- resources/views/student/exam.blade.php
+++ resources/views/student/exam.blade.php
@@ -28,7 +28,7 @@
         @foreach($exams as $id)
         @if ($id->date_start <= Carbon\Carbon::now('Asia/Manila')) 
         @if ($id->date_end > Carbon\Carbon::now('Asia/Manila')) 
-        @if($id->attempts > $exam_grades->attempt )
+        @if($id->attempts > $exam_grades['attempt'] )
         @if($quiz_latest->id == $exam_grades['quiz_attempt_id']) 
         @if($quiz_attempt->count() < $id->attempts)    
         <form action = "{{route('student.store.quiz_start',$id->id)}}" method="post" enctype="multipart/form-data">
