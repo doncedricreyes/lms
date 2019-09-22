6fd0df6326bbb3d57d3913a102451de5bd09e904
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-09-22 15:39:53 +0800

    Update exam-show.blade.php

--- resources/views/teacher/exam-show.blade.php
+++ resources/views/teacher/exam-show.blade.php
@@ -38,7 +38,7 @@
         @if($id->class_subject_teachers->teacher_id == Auth::user()->id)
         <a href="{{route('question.index',$id->id)}}" class="btn btn-primary">Add Questions</a>
         <a href="{{route('questions.show',$id->id)}}" class="btn btn-primary">View Questions</a>
-        <a href="{{route('exam.results',$id->id)}}" class="btn btn-primary">View Results</a>
+        <a href="/teacher/exams/{{$id->id}}/results?attempt=1" class="btn btn-primary">View Results</a>
         <a href="{{route('exam.edit',$id->id)}}" class="btn btn-primary">Edit</a>
         <a href="/teacher/exam/analysis/{{$id->id}}?attempt=1" class="btn btn-primary">Item Analysis</a>
         @endif
