fe97ef5a23f141575e1e8b6159905315e5f95e12
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-10 23:16:51 +0800

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -15,6 +15,15 @@
                       @endif
                     @endforeach
                   </div> <!-- end .flash-message -->
+                  @if ($errors->any())
+                  <div class="alert alert-danger">
+                      <ul>
+                          @foreach ($errors->all() as $error)
+                              <li>{{ $error }}</li>
+                          @endforeach
+                      </ul>
+                  </div>
+              @endif
                 <div class="container">
                     <div class="row">
             <div class="container">
@@ -23,7 +32,7 @@
                         
                         <div class="col-md-12">
                                 <legend>Admins</legend>
-                  <form action = "{{route('search_admin')}}" role="search" method="get"enctype="multipart/form-data">
+                                <form action = "{{route('search_admin')}}" role="search" method="get"enctype="multipart/form-data">
                                   <input type="text" class="form-control" name="search" id="search" placeholder="Search">
                                       </form>
                         <div class="table-responsive">
@@ -40,7 +49,7 @@
                                         <th>Role</th>
                                         @if(Auth::user()->role == 'superadmin')
                                         <th>Delete</th>
-                                        <th> <a href="{{url('admin/create')}}"class="btn btn-primary btn-xs">Add</a></th>                              
+                                        <th>             <th><a href="" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-xs">Add Admin </a></th></th>                              
                                         @endif
                                      
                                       
@@ -51,7 +60,7 @@
                             <tr>
                                 <td>{{$row->name}}</td>
                                 <td>{{$row->email}}</td>
-                                <td><p data-placement="top"  data-toggle="tooltip" title="Message"><a href="/admin/admins/{{$row->id}}/message"><button class="btn btn-primary btn-xs" data-title="View"><i class="glyphicon glyphicon-comment">
+                                <td><p data-placement="top"  data-toggle="tooltip" title="Message"><a href="admins/{{$row->id}}/message"><button class="btn btn-primary btn-xs" data-title="View"><i class="glyphicon glyphicon-comment">
                                 <td>{{$row->role}}</td>
                                
                                 @if(Auth::user()->role == 'superadmin')
@@ -80,7 +89,42 @@
 
 
 
-
+    <form action = "{{route('admin.create')}}" method="post"  enctype="multipart/form-data">
+      {{csrf_field() }}
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add Admin</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+              
+            <div class="form-group">
+               
+              <label>Name:</label>
+              <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
+            </div>
+            <div class="form-group">
+                  <label>Email:</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                      <label>Password:</label>
+                      <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+                    </div>
+     
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <input type="submit" class="btn btn-primary" value="Submit Information">
+        </div>
+      </div>
+    </div>
+  </div>
+  </form>
 
 
 
