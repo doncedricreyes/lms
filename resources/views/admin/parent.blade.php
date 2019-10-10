537e3fdb83401f414d253e34eea12936a5507d27
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-10 23:19:51 +0800

    Update parent.blade.php

--- resources/views/admin/parent.blade.php
+++ resources/views/admin/parent.blade.php
@@ -28,7 +28,7 @@
                         
                         <div class="col-md-12">
                         <legend>Parents</legend>
-                <form action = "{{route('search_parent')}}" role="search" method="get"enctype="multipart/form-data">
+                        <form action = "{{route('search_parent')}}" role="search" method="get"enctype="multipart/form-data">
                             <input type="text" class="form-control" name="search" id="search" placeholder="Search">
                                 </form>
                         <div class="table-responsive">
@@ -43,10 +43,9 @@
                                         <th>Username</th> 
                                         <th>Email</th>
                                         <th>View</th>
-                                        <th>Edit</th>
                                        <th>Delete</th>
                                        <th>Message</th>
-                                       <th> <a href="{{url('/admin/create/parents')}}"class="btn btn-primary btn-xs">Add</a></th>
+                                       <th> <a href="" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-xs">Add Parent </a></th>
                                        <th> <a href="{{url('admin/parents/export/excel')}}"class="btn btn-primary btn-xs">Export to Excel</a></th>
                                    </thead>
                     <tbody>
@@ -57,7 +56,7 @@
                                 <td>{{$row->username}}</td>
                                 <td>{{$row->email}}</td>
                                 <td><p data-placement="top" data-toggle="tooltip" title="View"><a href="/admin/parents/{{$row->id}}"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-zoom-in"></span></button></a></p></td>
-                                <td><p data-placement="top" data-toggle="tooltip" title="Edit"><a href="/admin/parents/{{$row->id}}/edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></a></p></td>
+                   
                              
                                 
                                 <form action="{{route('admin.parents.destroy',[$row->id])}}" method="POST">
@@ -65,19 +64,58 @@
                         {{ method_field('DELETE') }}
                     <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" onclick="return confirm('Are you sure?')" value="submit" type="submit" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
                     </form>
-                    <td><p data-placement="top"  data-toggle="tooltip" title="Message"><a href="/admin/parents/{{$row->id}}/message"><button class="btn btn-primary btn-xs" data-title="View"><i class="glyphicon glyphicon-comment">
+                    <td><p data-placement="top"  data-toggle="tooltip" title="Message"><a href="parents/{{$row->id}}/message"><button class="btn btn-primary btn-xs" data-title="View"><i class="glyphicon glyphicon-comment">
                 </tr>
                     @endforeach
-                    <div class="text-center">
-                    {{ $parents->links() }}
-                   
-                    </div>
+               
                    
                     
                     </tbody>
                         
                 </table>
+                <div class="text-center">
+                    {{ $parents->links() }}
+                   
+                    </div>
     </div>
+
+
+    <form action = "{{route('add-parent.store')}}" method="post"  enctype="multipart/form-data">
+        {{csrf_field() }}
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add Parent</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+                
+              <div class="form-group">
+                 
+                <label>Name:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Enter last name, first name middle initial">
+              </div>
+              <div class="form-group">
+                <label for="username"> Username:</label>
+                <input type="username" name="username" id="username" class="form-control" placeholder="Enter username">
+                  </div>
+                  <div class="form-group">
+                        <label>Password:</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+                      </div>
+       
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <input type="submit" class="btn btn-primary" value="Submit Information">
+          </div>
+        </div>
+      </div>
+    </div>
+    </form>
     </div>
 
 
