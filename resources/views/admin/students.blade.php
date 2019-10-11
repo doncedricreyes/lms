7a4189eed233be4b79d51e9440c25c8726a2ca4c
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-11 23:32:36 +0800

    Update students.blade.php

--- resources/views/admin/students.blade.php
+++ resources/views/admin/students.blade.php
@@ -31,7 +31,13 @@
                         <legend>Students</legend>
                         <div class="form-group">
                                 <form action = "{{route('search_student')}}" role="search" method="get"enctype="multipart/form-data">
-                            <input type="text" class="form-control" name="search" id="search" placeholder="Search">
+                                  <div>
+                            <input type="text" class="form-control" name="search" id="search" placeholder="Search"  style="width: 300px;">
+                            <br>
+                            <a href="" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-xs">Add Student </a>
+                            <a href="{{url('admin/students/export/excel')}}"class="btn btn-primary btn-xs">Export to Excel</a>
+                            <a href="" data-toggle="modal" data-target="#import" class="btn btn-primary btn-xs">Import</a>
+                                  </div>
                                 </form>
                           </div>
                         <div class="table-responsive">
@@ -48,8 +54,7 @@
                                         <th>View</th>
                                        <th>Delete</th>
                                        <th>Message</th>
-                                    <th><a href="" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-xs">Add Student </a></th>
-                                    <th> <a href="{{url('admin/students/export/excel')}}"class="btn btn-primary btn-xs">Export to Excel</a></th>
+                                
                                    </thead>
                     <tbody>
                     
@@ -127,6 +132,35 @@
       </div>
     </div>
     </form>
+
+  </div>
+  <form action = "{{route('student.import')}}" method="post"  enctype="multipart/form-data">
+    {{csrf_field() }}
+<div class="modal fade" id="import" tabindex="-1" role="dialog" aria-labelledby="importlabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="importlabel">Import File</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            
+          <div class="form-group">
+             
+            <label for="file_name">Choose file to upload:</label>
+            <input class="form-control" type="file" name="file_name" id="file_name" >
+        </div>
+   
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <input type="submit" class="btn btn-primary" value="Upload">
+      </div>
+    </div>
+  </div>
+</div>
+</form>
     </div>
 
 
