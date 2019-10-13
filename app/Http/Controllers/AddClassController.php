f31d3bab7a3d87deb3c3e3ff28866076cf9ff8c4
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-13 18:31:49 +0800

    Update AddClassController.php

--- app/Http/Controllers/AddClassController.php
+++ app/Http/Controllers/AddClassController.php
@@ -75,12 +75,12 @@ class AddClassController extends Controller
     {
         $input = request()->validate([
             'adviser_id' => 'required|unique:classes,adviser_id',
-            'year' => 'required',
-            'section' => 'required',
-            'section_name' => 'required',
-            'time' => 'required',
-            'room' => 'required',
-            'school_year' => 'required',
+            'year' => 'required|regex:/^[0-9]+$/u'|max:2,
+            'section' => 'required|regex:/^[0-9]+$/u|max:2',
+            'section_name' => 'required|regex:/^[a-zA-Z,. ]+$/u|max:255',
+            'time' => 'required|regex:/^[a-zA-Z-0-9 ]+$/u',
+            'room' => 'required|regex:/^[0-9]+$/u|max:3',
+            'school_year' => 'required|regex:/^[0-9- ]+$/u',
         ], [
 
 
