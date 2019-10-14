d3d980c0213828f7bf34c4d0f5528436caaeee9f
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-14 22:18:43 +0800

    Update archive_parent.blade.php

--- resources/views/admin/archive_parent.blade.php
+++ resources/views/admin/archive_parent.blade.php
@@ -43,7 +43,7 @@
                                         <th>Username</th> 
                                         <th>Email</th>
                                         <th>Status</th>
-                                      
+                                        <th>Edit</th>
                                       
                                    </thead>
                     <tbody>
@@ -54,6 +54,7 @@
                                 <td>{{$row->username}}</td>
                                 <td>{{$row->email}}</td>
                                 <td>{{$row->status}}</td>
+                                <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-id="{!! $row->id !!}" data-target="#edit-{{$row->id}}" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
                 </tr>
                     @endforeach
                
@@ -68,7 +69,46 @@
                     </div>
     </div>
 
+       @foreach($parents as $row)
+    <form action = "{{route('status_archive_parent', $row->id)}}" method="post" enctype="multipart/form-data">
+        
+        {{csrf_field() }}
+        <input name="_method" type="hidden" value="PUT">
+        <div class="modal fade" id="edit-{{$row->id}}" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="editLabel">Edit Parent</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="modal-body">
+          
+             
+          <div class="form-group">
+                 
+            <label for="name">Change Status:</label>
+            <select class="form-control select2" id="status" name="status" style="width: 100%;">
+                      
+              
+              <option value="inactive"> Inactive</option>
+              <option value="active"> Active</option>
+
+      </select>
+        </div>
    
+      </div>
+    
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <input type="submit" class="btn btn-primary" value="Submit Information">
+        </div>
+        </div>
+        </div>
+        </div>
+    </form>
+    @endforeach    
 </div>
 @endsection
 
