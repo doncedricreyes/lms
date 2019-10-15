95d69ae0813dcbcc3d0655e41e2e6f6cd6c12fa7
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-16 02:16:05 +0800

    Update exam-results.blade.php

--- resources/views/teacher/exam-results.blade.php
+++ resources/views/teacher/exam-results.blade.php
@@ -7,74 +7,56 @@
 @extends('layouts.user')
 
 <style>
-    .table-responsive {
-    min-height: .01%;
-    overflow-x: auto;
+        #submit{
+         
+            float: right;
+        }
+  
+    #searchbar{
+     
+      display: block;
+    text-align: center;
+    }
+   #search{
+     position: relative;
+     left: 37%;
+   }
+
+         .mdl-data-table th, td{
+  text-align: left !important;
+  font-size: 16px;
 }
-@media screen and (max-width: 767px) {
-    .table-responsive {
-        width: 100%;
-        margin-bottom: 15px;
-        overflow-y: hidden;
-        -ms-overflow-style: -ms-autohiding-scrollbar;
-        border: 1px solid #ddd;
-    }
-    .table-responsive > .table {
-        margin-bottom: 0;
-    }
-    .table-responsive > .table > thead > tr > th,
-    .table-responsive > .table > tbody > tr > th,
-    .table-responsive > .table > tfoot > tr > th,
-    .table-responsive > .table > thead > tr > td,
-    .table-responsive > .table > tbody > tr > td,
-    .table-responsive > .table > tfoot > tr > td {
-        white-space: nowrap;
-    }
-    .table-responsive > .table-bordered {
-        border: 0;
-    }
-    .table-responsive > .table-bordered > thead > tr > th:first-child,
-    .table-responsive > .table-bordered > tbody > tr > th:first-child,
-    .table-responsive > .table-bordered > tfoot > tr > th:first-child,
-    .table-responsive > .table-bordered > thead > tr > td:first-child,
-    .table-responsive > .table-bordered > tbody > tr > td:first-child,
-    .table-responsive > .table-bordered > tfoot > tr > td:first-child {
-        border-left: 0;
-    }
-    .table-responsive > .table-bordered > thead > tr > th:last-child,
-    .table-responsive > .table-bordered > tbody > tr > th:last-child,
-    .table-responsive > .table-bordered > tfoot > tr > th:last-child,
-    .table-responsive > .table-bordered > thead > tr > td:last-child,
-    .table-responsive > .table-bordered > tbody > tr > td:last-child,
-    .table-responsive > .table-bordered > tfoot > tr > td:last-child {
-        border-right: 0;
-    }
-    .table-responsive > .table-bordered > tbody > tr:last-child > th,
-    .table-responsive > .table-bordered > tfoot > tr:last-child > th,
-    .table-responsive > .table-bordered > tbody > tr:last-child > td,
-    .table-responsive > .table-bordered > tfoot > tr:last-child > td {
-        border-bottom: 0;
-    }
+#head {
+  background-color:#488cc7;
+  text-align: center !important;
+  font-size: 28px;
+  color: white;
 }
-</style>
+#table{
+  background-color:snow;
+}
+  </style>
 @section('content')
 @if($exams->get(0)->class_subject_teachers->teacher_id == auth::user()->id)
 <div class="container" id="view">
  
-        
+        @isset($grades)
     <div class="row">
-        <div class="col-lg-12 col-md-offset-0">
-        <div class="panel panel-default">
-            <div class="panel-heading">Results
-             <form action="{{route('exam.results',$exams->get(0)->id)}}" method="GET">
+             <div class="col-lg-12 col-md-offset-0">
+                    <div class="panel panel-default">
+            <div class="panel-heading" id="head">Results</div>
+            <br>
+                <form action="{{route('exam.results',$exams->get(0)->id)}}" method="GET">
+                        <div id="searchbar">
                 <select  name="attempt" id="attempt" onchange="this.form.submit()">
                     <option disabled selected value> -- select attempt -- </option>
                    @for($i=1; $i <= $exams->get(0)->attempts;$i++)
                  <option value={{$i}}>Attempt {{$i}}</option>
                  @endfor
                </select>
+                        </div>
             </form>
-            </div>
+            
 
             <div class="panel-body">   
                     <div class = "table-responsive"> 
@@ -113,6 +95,7 @@
 
 </div>
 @endif
+@endisset
 @endsection
 
             
