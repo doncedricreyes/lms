ecca1c145a94b24e4cce8bd5f01f30983a050190
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-16 01:08:17 +0800

    Update account.blade.php

--- resources/views/student/account.blade.php
+++ resources/views/student/account.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
 <style>
-    #submit{
-        position: absolute;
-        right: 5%;
-        top: 100%;
+     #submit{
+        position: relative;
+        left: 46%;
+    }
+    #view{
+        position: relative;
+        left: 5%;
     }
     </style>
 <div class="container" id="view">
@@ -30,7 +33,7 @@
         {{csrf_field() }}
 <div class="form-group">
     <label>Email:</label>
-    <input class="form-control" type="email" id="email" name="email" value="{{$student->get(0)->email}}">
+    <input class="form-control" type="email" id="email" name="email" value="{{$student->get(0)->email}}" style="width:700px;">
   </div>
 
 <div class="col-lg-12 p-t-20">
@@ -43,10 +46,10 @@
 <form action="{{route('student.edit.pass')}}" method="post" enctype="multipart/form-data">
     {{csrf_field() }}
      <div class="form-group has-feedback">
-    <input id="oldpassword" type="password" placeholder="Password" class="form-control" name="oldpassword" required>
+    <input id="oldpassword" type="password" placeholder="Password" class="form-control" name="oldpassword" required style="width:700px;">
     </div>
     <div class="form-group has-feedback">
-            <input id="password" type="password" placeholder="New Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+            <input id="password" type="password" style="width:700px;" placeholder="New Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
         
             @if ($errors->has('password'))
                 <span class="invalid-feedback" role="alert">
@@ -56,7 +59,7 @@
  
           </div>
           <div class="form-group has-feedback">
-                <input id="password-confirm" type="password" placeholder="Confirm Password" class="form-control" name="password_confirmation" required>
+                <input id="password-confirm" type="password" style="width:700px;" placeholder="Confirm Password" class="form-control" name="password_confirmation" required>
           
               </div>
               <div class="form-group has-feedback">
