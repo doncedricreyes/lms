8947c714ec3903e4c25d8a01f9d8ba2ff30fe7a3
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-15 23:52:00 +0800

    Update exam.blade.php

--- resources/views/student/exam.blade.php
+++ resources/views/student/exam.blade.php
@@ -5,15 +5,32 @@
 
 
 @extends('layouts.user')
-
+<style>
+    .demo-card-wide.mdl-card {
+      position: relative; 
+      left: 5%;
+      width: 90%;
+    
+     
+    }
+  
+    </style>
 @section('content')
 
 <div class="container" id="view">
  
-  <?php  $i = $exam_grades ?>
-  @foreach($exams as $exam)
-            <legend><h1>{{$exam->title}}</h1></legend>
-            @endforeach
+ <?php  $i = $exam_grades ?>
+    <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+        <div class="mdl-card__title" style=" background-color:#488cc7;">
+          <h2 class="mdl-card__title-text" style="font-size:28px; color: white;"> 
+              @foreach($exams as $exam)
+              {{$exam->title}}
+          @endforeach
+                            </h2>
+        </div>
+ 
+ <div class="mdl-card__actions mdl-card--border" style=" background-color:snow;">
+            <div class="mdl-card__supporting-text">
             @foreach($exams as $exam)
 <h3>This exam will open on {{$exam->date_start}}
 <h3>This exam will closed on {{$exam->date_end}}
@@ -22,10 +39,9 @@
 <h3>Attempts: {{$exam->attempts}}</h3>
 <h3>Your Attempt: {{$quiz_attempt->count()}}</h3>
 <h3>Time: {{$exam->time/60}} minutes</h3>
-
-
           @endforeach
-        @foreach($exams as $id)
+          
+            @foreach($exams as $id)
         @if ($id->date_start <= Carbon\Carbon::now('Asia/Manila')) 
         @if ($id->date_end > Carbon\Carbon::now('Asia/Manila')) 
         @if($id->attempts > $exam_grades['attempt'] )
@@ -48,6 +64,11 @@
 @endif
         <a href="{{route('student.show.result',$id->id)}}" class="btn btn-primary">View Results</a>
           @endforeach
+          
+               </div>
+          </div>
+        </div>
+      
 </div>
 @endsection
 
