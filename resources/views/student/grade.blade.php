3e5ebb0253d40aa26a7c48d039e2094f86a8cdc6
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-15 23:18:22 +0800

    Update grade.blade.php

--- resources/views/student/grade.blade.php
+++ resources/views/student/grade.blade.php
@@ -1,17 +1,45 @@
 
 @extends('layouts.user')
-
+<style>
+        #searchbar{
+         
+          display: block;
+        text-align: center;
+        }
+       #search{
+         position: relative;
+         left: 37%;
+       }
+    
+             .mdl-data-table th, td{
+      text-align: left !important;
+      font-size: 16px;
+    }
+    #head {
+      background-color:#488cc7;
+      text-align: center !important;
+      font-size: 28px;
+      color: white;
+    }
+    #table{
+      background-color:snow;
+    }
+      </style>
 @section('content')
             <div class="container">
                     <div class="row">
                         
                         
-                        <div class="col-md-12">
-                        <legend>Grades</legend>
-                        <div class="table-responsive">
-                
-                                
-                              <table id="mytable" class="table table-bordred table-striped">
+                            <div  class="col-lg-12 col-md-offset-0">
+                                    <div id="table" class="panel panel-default">
+                                        <div class="panel-heading" id="head">Grades</div>
+                                        <br>
+                                        
+                                        <div  class="panel-body"> 
+                                                <div  class="table-responsive">
+                                        
+                                                        
+                                                  <table  class="mdl-data-table mdl-js-data-table col-lg-12" >
                                    
                                    <thead>
                                    
