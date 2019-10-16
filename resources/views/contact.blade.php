d9e69b7b4b24d5b9e23652906ed8ae385090e2b6
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-17 01:30:36 +0800

    Create contact.blade.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+        <link rel="shortcut icon" type="image/x-icon" href="/storage/images/logo3.ico">
+
+    <title>Contact | SCCV-LMS</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    
+        <!-- FontAwesome CSS -->
+        <link rel="stylesheet" href="{{asset('bower_components/font-awesome/css/font-awesome.min.css')}}">
+    
+        <!-- ElegantFonts CSS -->
+        <link rel="stylesheet" href="{{asset('bower_components/elegant-fonts.css')}}">
+    
+        <!-- themify-icons CSS -->
+        <link rel="stylesheet" href="{{asset('bower_components/themify-icons.css')}}">
+    
+        <!-- Swiper CSS -->
+        <link rel="stylesheet" href="{{asset('bower_components/swiper.min.css')}}">
+    
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('bower_components/style.css')}}">
+
+    <style>  
+
+
+            img {
+          position: absolute;
+          left: 50px;
+          top: -13px;
+          z-index: -1;
+        
+        
+          
+        }
+
+    </style>  
+
+</head>
+<body class="contact-page">
+    <div class="hero-content">
+            <header class="site-header">
+                    <header class="site-header">
+                    
+                            
+        
+                        <div class="nav-bar">
+                            <div>
+                                <div class="row">
+                                        <div class="col-9 col-lg-3">
+                                                <img src="/storage/images/logo3.png" width="80" height="80">
+                                             <div class="site-branding">                       
+                                                 <h3 class="site-title"><a href="/" rel="home"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SCCV-<span>LMS</span></a></h3>
+                                             
+                                             </div><!-- .site-branding -->
+                                         </div><!-- .col -->
+        
+                                    <div class="col-3 col-lg-9 justify-content-end align-content-center">
+                                            <nav class="site-navigation flex justify-content-end align-items-center">
+                                                <ul class="flex flex-column flex-lg-row justify-content-end align-content-center">
+                                                    <li><a href="/"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home </span></a></li>
+                                                    <li><a href="about"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> About</a></li>                   
+                                                    <li class="current-menu-item"><a href="contact"><span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> Contact</a></li>  
+                                                    <li><a href="learnmore"><span class="glyphicon glyphicon-flag" aria-hidden="true"></span> Learn More</a></li>
+                                                    <li><a href="/student/login"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Login </a></li>
+                                                    
+                                                </ul>                                       
+                                                </ul>
+                
+                                              <div class="hamburger-menu d-md-none">
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                </div>
+                
+                                                <div class="header-bar-cart">
+                                                    <a href="#" class="flex justify-content-center align-items-center"><span aria-hidden="true"></span></a>
+                                                </div> 
+                
+                                                
+                                            </nav><!-- .site-navigation -->
+                                        </div><!-- .col -->
+                                </div><!-- .row -->
+                            </div><!-- .container -->
+                        </div><!-- .nav-bar -->
+                    </header><!-- .site-header -->
+                </header><!-- .site-header -->
+
+        <div class="page-header-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <header class="entry-header" id="contact">
+                            <h1>CONTACT US!</h1>
+                        </header><!-- .entry-header -->
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .container -->
+        </div><!-- .page-header-overlay -->
+    </div><!-- .page-header -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumbs">
+                    <ul class="flex flex-wrap align-items-center p-0 m-0">
+                        <li><a href="/"><i class="fa fa-home"></i> Home</a></li>
+                        <li>Contact</li>
+                    </ul>
+                </div><!-- .breadcrumbs -->
+            </div><!-- .col -->
+        </div><!-- .row -->
+
+        <div class="row justify-content-between">
+            <div class="col-12">
+                <div class="contact-gmap">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12058.914601470693!2d121.00900503189247!3d14.704856932274676!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2d8cd5148b63907a!2sSt.%20Catherine%20College%20of%20Valenzuela%20Inc.!5e0!3m2!1sen!2sph!4v1571150480890!5m2!1sen!2sph" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe></iframe>
+                </div><!-- .contact-gmap -->
+            </div><!-- .col -->
+
+            <div class="col-12 col-lg-6">
+                <div class="contact-form">
+                    <h3>Contact Form</h3>
+
+                    <form>
+                        <input type="text" placeholder="Your Name">
+                        <input type="email" placeholder="Your Email">
+                        <input type="text" placeholder="Subject">
+                        <textarea placeholder="Your Message" rows="4"></textarea>
+                        <input type="submit" value="Send Message">
+                    </form>
+                </div><!-- .contact-form -->
+            </div><!-- .col -->
+
+            <div class="col-12 col-lg-6">
+                <div class="contact-info">
+                    <h3>Contact Information</h3>
+
+                    <p>Office hours: 8:00 AM - 6:00 PM </p>
+
+                    <ul class="p-0 m-0">
+                        <li><span>Location:</span>Sto. Rosario Street, Mapulang Lupa, 1448 Valenzuela City, Metro Manila</li>
+                        <li><span>Email:</span><a href="#">st.catherinecollegeofvalenzuela@yahoo.com</a></li>
+                        <li><span>Phone:</span><a href="#">(02) 443 1860</a></li>
+                    </ul>
+                </div><!-- .contact-info -->
+            </div><!-- .col -->
+        </div><!-- .row -->
+    </div><!-- .container -->
+
+   <br><br><br><br>
+
+   
+    
+            <div class="footer-bar">
+                <div class="container">
+                        <footer class="site-footer">
+                                <div class="footer-widgets">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-12 col-md-6 col-lg-3">
+                                                <div class="foot-about">
+                                                        
+                                                <a class="foot-logo" href="#"><img src="/storage/images/logo3.png" width="90" height="90"><br></a>
+                        
+                                                    <p>This Learning Management System is for St. Catherine College of Valenzuela only. </p>
+                        
+                                                    <p class="footer-copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
+                                                </div><!-- .foot-about -->
+                                            </div><!-- .col -->
+                        
+                                            <div class="col-12 col-md-6 col-lg-3 mt-5 mt-md-0">
+                                                <div class="foot-contact">
+                                                    <h2>Contact Us</h2>
+                        
+                                                    <ul>
+                                                        <li>Email: st.catherinecollegeof<br>valenzuela@yahoo.com</li>
+                                                        <li>Phone: (02) 443 1860</li>
+                                                        <li>Address: Sto. Rosario Street, Mapulang Lupa, 1448 Valenzuela City, Metro Manila</li>
+                                                    </ul>
+                                                </div><!-- .foot-contact -->
+                                            </div><!-- .col -->
+                        
+                                 
+                                            <div class="col-12 col-md-6 col-lg-3 mt-5 mt-lg-0">
+                                                <div class="quick-links flex flex-wrap">
+                                                    <h2 class="w-100">Quick Links</h2>
+                        
+                                                    <ul class="w-50" >
+                                                        <li style="position:relative;right:100%;"><a href="about">About </a></li>
+                                                        <li style="position:relative;right:100%;"><a href="contact">Contact </a></li>
+                                                        <li style="position:relative;right:100%;"><a href="learnmore">Learn More</a></li>
+                           <!--login page-->            <li style="position:relative;right:100%;"><a href="/student/login">Login</a></li>
+                                                    </ul>
+                        
+                                                    
+                                                </div><!-- .quick-links -->
+                                            </div><!-- .col -->
+                        
+                                            <div class="col-12 col-md-6 col-lg-3 mt-5 mt-lg-0">
+                                                <div class="follow-us">
+                                                    <h2>Follow Us</h2>
+                        
+                                                    <ul class="follow-us flex flex-wrap align-items-center">
+                                                        <li><a href="https://www.facebook.com/sccvofficial"><i class="fa fa-facebook"></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                                    </ul>
+                                                </div><!-- .quick-links -->
+                                            </div><!-- .col -->
+                                        </div><!-- .row -->
+                                    </div><!-- .container -->
+                                </div><!-- .footer-widgets -->
+                    
+                </div><!-- .container -->
+
+                
+            </div><!-- .footer-bar -->
+        </footer><!-- .site-footer -->
+
+       
+        <script src="{{asset('bower_components/jquery.js')}}"></script>
+        <script src="{{asset('bower_components/swiper.min.js')}}"></script>
+        <script src="{{asset('bower_components/masonry.pkgd.min.js')}}"></script>
+        <script src="{{asset('bower_components/jquery.collapsible.min.js')}}"></script>
+        <script src="{{asset('bower_components/custom.js')}}"></script>
+</body>
+</html>
