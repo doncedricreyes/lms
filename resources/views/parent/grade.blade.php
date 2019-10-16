347e2371f0e4a74c7e997aa3b2213999efc6d979
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-16 17:03:17 +0800

    Update grade.blade.php

--- resources/views/parent/grade.blade.php
+++ resources/views/parent/grade.blade.php
@@ -17,14 +17,14 @@
 <div class="container" id ="view">
     @foreach($students as $class_student)
    <div class="demo-card-wide mdl-card mdl-shadow--2dp">
-      <div class="mdl-card__title">
+      <div class="mdl-card__title" style=" background-color:#488cc7;">
         <h2 class="mdl-card__title-text"> 
-           <a href="grades/{{$class_student->student_id}}"> <h1>
+           <a href="grades/{{$class_student->student_id}}"> <h1 style="font-size:28px; color: white;">
          {{$class_student->students->get(0)->name}}</h1></a>
                           </h2>
       </div>
       
-      <div class="mdl-card__actions mdl-card--border">
+      <div class="mdl-card__actions mdl-card--border" style=" background-color:snow;">
         <div class="mdl-card__supporting-text">
             <h4>Adviser: {{$class_student->class_subject_teachers->get(0)->classes->get(0)->teachers->get(0)->name}}</h4>
                 <h4>Year and Section: {{$class_student->class_subject_teachers->get(0)->classes->get(0)->year}}-{{$class_student->class_subject_teachers->get(0)->classes->get(0)->section}}</h4>
