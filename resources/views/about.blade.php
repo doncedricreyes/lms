6e9de5d37826b1c68d5afe77c5adb9cd8d7b2b08
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-17 01:30:16 +0800

    Create about.blade.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>About | SCCV-LMS</title>
+    
+
+  
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+
+  <!-- FontAwesome CSS -->
+  <link rel="stylesheet" href="{{asset('bower_components/font-awesome/css/font-awesome.min.css')}}">
+
+  <!-- ElegantFonts CSS -->
+  <link rel="stylesheet" href="{{asset('bower_components/elegant-fonts.css')}}">
+
+  <!-- themify-icons CSS -->
+  <link rel="stylesheet" href="{{asset('bower_components/themify-icons.css')}}">
+
+  <!-- Swiper CSS -->
+  <link rel="stylesheet" href="{{asset('bower_components/swiper.min.css')}}">
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{asset('bower_components/style.css')}}">
+
+    <style>  
+
+
+            img {
+          position: absolute;
+          left: 50px;
+          top: -13px;
+          z-index: -1;
+        
+        
+          
+        }
+            
+         </style>
+
+</head>
+<body class="about-page">
+   
+    <div class="hero-content">
+       <header class="site-header">
+            
+
+            <div class="nav-bar">
+                <div>
+                    <div class="row">
+                        <div class="col-9 col-lg-3">
+                               <img src="/storage/images/logo3.png" width="80" height="80">
+                            <div class="site-branding">                       
+                                <h3 class="site-title"><a href="/html" rel="home">&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SCCV-<span>LMS</span></a></h3>
+                            
+                            </div><!-- .site-branding -->
+                        </div><!-- .col -->
+                    
+
+                        <div class="col-3 col-lg-9 justify-content-end align-content-center">
+                            <nav class="site-navigation flex justify-content-end align-items-center">
+                                <ul class="nav navbar-nav justify-content-end align-content-center">
+                                    <li ><a href="/"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home </span></a></li>
+                                    <li class="current-menu-item"><a href="about"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> About</a></li>                   
+                                    <li><a href="contact"><span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> Contact</a></li>  
+                                    <li><a href="learnmore"><span class="glyphicon glyphicon-flag" aria-hidden="true"></span> Learn More</a></li>
+  <!--yung login button-->          <li><a href="/student/login"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Login </a></li>
+   <!--pero login lang, pipili pa sila sa mismong login page ng role(?) 
+    or kung gusto mo lagyan mo na lang dropdown hah-->                                 
+                                </ul>                                       
+                                </ul>
+
+                              <div class="hamburger-menu d-md-none"  >
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+
+                           
+
+                                
+                            </nav><!-- .site-navigation -->
+                        </div><!-- .col -->
+                    </div><!-- .row -->
+                </div><!-- .container -->
+            </div><!-- .nav-bar -->
+        </header><!-- .site-header -->
+
+        <div class="page-header-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <header class="entry-header" id="about">
+                            <h1>ABOUT</h1>
+                        </header><!-- .entry-header -->
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .container -->
+        </div><!-- .page-header-overlay -->
+    </div><!-- .page-header -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumbs">
+                    <ul class="flex flex-wrap align-items-center p-0 m-0">
+                        <li><a href="/"><i class="fa fa-home"></i> Home</a></li>
+                        <li>About</li>
+                    </ul>
+                </div><!-- .breadcrumbs -->
+            </div><!-- .col -->
+        </div><!-- .row -->
+
+        <div class="row">
+            <div class="col-12">
+                <div class="about-heading">
+                    <h2 class="entry-title">Welcome to St. Catherine College of Valenzuela Learning Management System</h2>
+
+                    <p>You are about to know more about this growing website</p>
+                </div><!-- .about-heading -->
+            </div><!-- .col -->
+
+            <div class="col-12 col-lg-12">
+                <div class="about-stories">
+                    <h3>Learning Management System</h3>
+
+                    <p>Learning Management System is widely used in Universities in the Philippines</p>
+
+                    <p>Our team decided to wide the knowledge of Highschool students if what is the role of <br> Learning Management System in our education system. </p>
+
+                    <ul class="p-0 m-0 green-ticked">
+                        <li>Easy to use</li>
+                        <li>Provides unlimited access to eLearning materials</li>
+                        <li>Reduces Learning costs</li>
+                        <li>Secured</li>
+                        <li> FREE! </li>
+                        
+                    </ul><!-- .green-ticked -->
+                </div><!-- .about-stories -->
+            </div><!-- .col -->
+
+            
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="team-heading">
+                    <h2 class="entry-title">Meet Our Team</h2>
+                    <p>We are the 5th year Computer Engineering students of Polytechnic University of the Philippines
+                        <br> Feel free to get in touch with us! 
+                    </p>
+                </div><!-- .team-heading -->
+            </div><!-- .col -->
+
+
+<br> <br> <br>
+            <div class="col-12 col-md-6">
+                    <div class="team-member">
+                        <img src="/storage/images/userDon.PNG" width="150" height="170"  alt="" style="position:relative;left:0%;">
+    
+                        <h3> Don Cedric Reyes</h3>
+                        <h4>Head Programmer</h4>
+    
+                        <ul class="p-0 m-0 d-flex justify-content-center align-items-center" style="position:relative;left:35%;">
+                            <li><a href="https://www.facebook.com/doncedric.reyes"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                    </div><!-- .team-member -->
+                </div><!-- .col -->
+
+                <div class="col-12 col-md-6">
+                        <div class="team-member">
+                            <img src="/storage/images/userMon.jpg" class="img-rounded" alt="" style="position:relative;left:0%;">
+        
+                            <h3> Monica Gagarra</h3>
+                            <h4>Assistant Programmer</h4>
+        
+                            <ul class="p-0 m-0 d-flex justify-content-center align-items-center" style="position:relative;left:35%;">
+                                <li><a href="https://www.facebook.com/monicaaaggrr"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.instagram.com/monicaa_sy/"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="https://twitter.com/ggrrmonicaaa"><i class="fa fa-twitter"></i></a></li>
+                            </ul>
+                            
+                        </div><!-- .team-member -->
+                    </div><!-- .col -->
+
+                    
+        </div><!-- .row -->
+        <br><br><br>
+    </div><!-- .container -->
+
+    
+
+    <footer class="site-footer">
+   
+    
+            <div class="footer-bar">
+                <div class="container">
+                        <div class="footer-widgets">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-12 col-md-6 col-lg-3">
+                                            <div class="foot-about">
+                                                    
+                                            <a class="foot-logo" href="#"><img src="/storage/images/logo3.png" width="90" height="90"><br></a>
+                    
+                                                <p>This Learning Management System is for St. Catherine College of Valenzuela only. </p>
+                    
+                                                <p class="footer-copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
+                                            </div><!-- .foot-about -->
+                                        </div><!-- .col -->
+                    
+                                        <div class="col-12 col-md-6 col-lg-3 mt-5 mt-md-0">
+                                            <div class="foot-contact">
+                                                <h2>Contact Us</h2>
+                    
+                                                <ul>
+                                                    <li>Email: st.catherinecollegeof<br>valenzuela@yahoo.com</li>
+                                                    <li>Phone: (02) 443 1860</li>
+                                                    <li>Address: Sto. Rosario Street, Mapulang Lupa, 1448 Valenzuela City, Metro Manila</li>
+                                                </ul>
+                                            </div><!-- .foot-contact -->
+                                        </div><!-- .col -->
+                    
+                                        <div class="col-12 col-md-6 col-lg-3 mt-5 mt-lg-0">
+                                            <div class="quick-links flex flex-wrap">
+                                                <h2 class="w-100">Quick Links</h2>
+                    
+                                                <ul class="w-50" >
+                                                    <li style="position:relative;right:100%;"><a href="about">About </a></li>
+                                                    <li style="position:relative;right:100%;"><a href="contact">Contact </a></li>
+                                                    <li style="position:relative;right:100%;"><a href="learnmore">Learn More</a></li>
+                       <!--login page-->            <li style="position:relative;right:100%;"><a href="/student/login">Login</a></li>
+                                                </ul>
+                    
+                                                
+                                            </div><!-- .quick-links -->
+                                        </div><!-- .col -->
+                    
+                                        <div class="col-12 col-md-6 col-lg-3 mt-5 mt-lg-0">
+                                            <div class="follow-us">
+                                                <h2>Follow Us</h2>
+                    
+                                                <ul class="follow-us flex flex-wrap align-items-center">
+                                                    <li><a href="https://www.facebook.com/sccvofficial"><i class="fa fa-facebook"></i></a></li>
+                                                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                                </ul>
+                                            </div><!-- .quick-links -->
+                                        </div><!-- .col -->
+                                    </div><!-- .row -->
+                                </div><!-- .container -->
+                            </div><!-- .footer-widgets -->
+                    
+                </div><!-- .container -->
+            </div><!-- .footer-bar -->
+        </footer><!-- .site-footer -->
+
+    <script type='text/javascript' src='js/jquery.js'></script>
+    <script type='text/javascript' src='js/swiper.min.js'></script>
+    <script type='text/javascript' src='js/masonry.pkgd.min.js'></script>
+    <script type='text/javascript' src='js/jquery.collapsible.min.js'></script>
+    <script type='text/javascript' src='js/custom.js'></script>
+
+</body>
+</html>
