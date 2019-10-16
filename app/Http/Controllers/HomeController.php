234629ba31c86d756651935a65a1c648e63dcf46
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-17 01:33:41 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+    
     }
 
     /**
@@ -23,6 +23,24 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('index');
     }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    
+    public function learnmore()
+    {
+        return view('learnmore');
+    }
+
+
 }
