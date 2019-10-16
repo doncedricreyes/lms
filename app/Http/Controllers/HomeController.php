7eb7ab7ce1f45d090b94fe5e86c3148fc77dc140
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-17 02:53:18 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Contact;
 class HomeController extends Controller
 {
     /**
@@ -42,5 +42,29 @@ class HomeController extends Controller
         return view('learnmore');
     }
 
+    public function message(Request $request)
+    {
+        $input = request()->validate([
+            'name' => 'required|string|max:255|',
+            'email' => 'required|string|max:255|email',
+            'subject' => 'required|string|',
+            'message' => 'required|string|',
+        ], [
+
+      
+            
+            
+
+        ]);
+
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->subject = $request->subject;
+        $contact->message = $request->message;
+        $contact->save();
+        $request->session()->flash('alert-success', 'Message Sent');
+        return redirect()->back();
+    }
 
 }
