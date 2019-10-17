1879854d16652249f8851f91caf572c2e6140f18
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-17 23:12:05 +0800

    Update account.blade.php

--- resources/views/admin/account.blade.php
+++ resources/views/admin/account.blade.php
@@ -3,13 +3,15 @@
 @section('content')
 <style>
     #submit{
-        position: absolute;
-        right: 5%;
-        top: 100%;
+        position: relative;
+        left: 46%;
+    }
+    #view{
+        position: relative;
+        left: 5%;
     }
     </style>
-<div class="container" id="view">
-        <div class="flash-message">
+          <div class="flash-message">
                 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
                   @if(Session::has('alert-' . $msg))
             
@@ -26,11 +28,13 @@
                   </ul>
               </div>
           @endif
+<div class="container" id="view">
+  
     <form action="{{route('admin.edit.email')}}" method="post" enctype="multipart/form-data">
         {{csrf_field() }}
 <div class="form-group">
     <label>Email:</label>
-    <input class="form-control" type="email" id="email" name="email" value="{{$admin->get(0)->email}}">
+    <input class="form-control" type="email" id="email" name="email" value="{{$admin->get(0)->email}}" style="width:700px;">
   </div>
 
 <div class="col-lg-12 p-t-20">
@@ -43,10 +47,10 @@
 <form action="{{route('admin.edit.pass')}}" method="post" enctype="multipart/form-data">
     {{csrf_field() }}
        <div class="form-group has-feedback">
-    <input id="oldpassword" type="password" placeholder="Password" class="form-control" name="oldpassword" required>
+    <input id="oldpassword" type="password" placeholder="Password" class="form-control" name="oldpassword" required style="width:700px;">
     </div>
     <div class="form-group has-feedback">
-            <input id="password" type="password" placeholder="New Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+            <input id="password" style="width:700px;" type="password" placeholder="New Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
         
             @if ($errors->has('password'))
                 <span class="invalid-feedback" role="alert">
@@ -56,7 +60,7 @@
  
           </div>
           <div class="form-group has-feedback">
-                <input id="password-confirm" type="password" placeholder="Confirm Password" class="form-control" name="password_confirmation" required>
+                <input id="password-confirm" style="width:700px;" type="password" placeholder="Confirm Password" class="form-control" name="password_confirmation" required>
           
               </div>
               <div class="form-group has-feedback">
